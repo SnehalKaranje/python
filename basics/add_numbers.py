33deda6bcf12afa575615e667ef4de8eebaf4c28
SnehalKaranje <snehalkaranje9@gmail.com>
2021-07-27 21:19:16 +0530

    basics - addition of two numbers

---
+++ basics/add_numbers.py
@@ -0,0 +1,14 @@
+def Add(num1, num2):
+	return int(num1) + int(num2);
+	
+def main():
+	if(__name__ == "__main__"):
+		print("Enter first number:");
+		num1 = input();
+		
+		print("Enter second number:");
+		num2 = input();
+		
+		print("Addition is:", Add(num1, num2));
+		
+main();

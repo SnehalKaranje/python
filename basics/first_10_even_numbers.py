e7bfff38b8b61fa6187ef109f4ea40e8fc620a7c
SnehalKaranje <snehalkaranje9@gmail.com>
2021-07-27 21:27:24 +0530

    basics - first 10 even numbers

---
+++ basics/first_10_even_numbers.py
@@ -0,0 +1,9 @@
+def displayEvenNumbers(count = 10):
+	for num in range(2, count*2+1, 2):
+		print(num, end = ' ');
+
+def main():
+	if(__name__ == "__main__"):
+		displayEvenNumbers();
+			
+main();

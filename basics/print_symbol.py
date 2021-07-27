f3ba6751ab33c3f5e4d7c5138ee9eac78394a0a8
SnehalKaranje <snehalkaranje9@gmail.com>
2021-07-27 21:26:30 +0530

    basics - print symbol

---
+++ basics/print_symbol.py
@@ -0,0 +1,11 @@
+def printPattern(num):
+	for i in range(int(num)):
+		print("*", end = ' ');
+
+def main():
+	if(__name__ == "__main__"):
+		print("Enter number:");
+		num = input();
+		printPattern(num);
+			
+main();

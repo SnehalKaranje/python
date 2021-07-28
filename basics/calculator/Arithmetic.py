4d5a9a794bbadd60f80bc26d16bf686cce4e7bc3
SnehalKaranje <snehalkaranje9@gmail.com>
2021-07-28 08:15:18 +0530

    basics - arithmetic operations

---
+++ basics/calculator/Arithmetic.py
@@ -0,0 +1,11 @@
+def add(num1, num2):
+	return int(num1) + int(num2);
+
+def sub(num1, num2):
+	return int(num1) - int(num2);
+	
+def mult(num1, num2):
+	return int(num1) * int(num2);
+
+def div(num1, num2):
+	return int(num1) / int(num2);
---
+++ basics/calculator/main.py
@@ -0,0 +1,16 @@
+from Arithmetic import *;
+	 
+def main():
+	if(__name__ == "__main__"):
+		print("Enter first number:");
+		num1 = input();
+	
+		print("Enter second number:");
+		num2 = input();
+	
+		print("Addition is:", add(num1, num2));
+		print("Subtraction is:", sub(num1, num2));
+		print("Multiplication is:", mult(num1, num2));
+		print("Division is:", div(num1, num2));
+	
+main();
